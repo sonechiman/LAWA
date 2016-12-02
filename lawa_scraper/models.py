2902661a853aaeddd2a8fefacb7c0cfba46071bb
Yuya Soneoka <ej.a9p96@gmail.com>
2016-12-02 12:48:29 +0900

    Add diff model

--- lawa_scraper/models.py
+++ lawa_scraper/models.py
@@ -18,3 +18,22 @@ class Webpage(Base):
     def __repr__(self):
         return "<Webpage(id={0}, company={1}, timestamp={2})>".format(
             self.id, self.company, self.timestamp)
+
+
+class Diff(Base):
+    __tablename__ = 'diff'
+
+    id = Column(Integer, primary_key=True)
+    diff = Column(Text, nullable=False)
+    original_url = Column(String, nullable=False, index=True, unique=True)
+    path_label = Column(String, index=True, nullable=False)
+    company = Column(String, nullable=False, index=True)
+    date = Column(DateTime, nullable=False, index=True)
+    delete_lines = Column(Integer)
+    add_lines = Column(Integer)
+    princing = Column(Integer)
+    free = Column(Integer)
+
+    def __repr__(self):
+        return "<Diff(id={0}, company={1}, timestamp={2})>".format(
+            self.id, self.company, self.date)
